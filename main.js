01b26f3833506197e52b97456188ac25e6397b3b
Mohammed-Nur-030 <126596779+Mohammed-Nur-030@users.noreply.github.com>
2024-01-13 00:53:29 +0530

    Dockerizing the nodejs

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 const express=require('express')
 const app=express();
 
-const PORT=process.env.PORT||8000
+const PORT=process.env.PORT|| 8000
 
 app.get('/',(req,res)=>{
     return res.json({message:"Hey I m Nodejs in Container"})
 })
-app.get(PORT,()=>console.log(`Server Listening at Port ${PORT}`))
+app.listen(PORT,()=>console.log(`Server Listening at Port ${PORT}`))
